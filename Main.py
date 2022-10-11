7f5eb00cbd5f5edc0514024216f5e16fc0ba7972
URK21CS1159 <110010698+URK21CS1159@users.noreply.github.com>
2022-10-11 12:17:28 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,43 +1,32 @@
 from typing import List
 
-def merge_sort(data) -> None:
-  # Write code here
-   if len(data) > 1:
+def merge_sort(data):
+    if len(data)>1:
+        mid = len(data)//2
+        lefthalf = data[:mid]
+        righthalf = data[mid:]
 
-        #  r is the point where the array is divided into two subarrays
-        r = len(data)//2
-        L = data[:r]
-        M = data[r:]
-
-        # Sort the two halves
-        mergeSort(L)
-        mergeSort(M)
-
-        i = j = k = 0
-
-        # Until we reach either end of either L or M, pick larger among
-        # elements L and M and place them in the correct position at A[p..r]
-        while i < len(L) and j < len(M):
-            if L[i] < M[j]:
-                data[k] = L[i]
-                i += 1
+        merge_sort(lefthalf)
+        merge_sort(righthalf)
+        i=j=k=0      
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                data[k]=lefthalf[i]
+                i=i+1
             else:
-                data[k] = M[j]
-                j += 1
-            k += 1
+                data[k]=righthalf[j]
+                j=j+1
+            k=k+1
 
-        # When we run out of elements in either L or M,
-        # pick up the remaining elements and put in A[p..r]
-        while i < len(L):
-            data[k] = L[i]
-            i += 1
-            k += 1
+        while i < len(lefthalf):
+            data[k]=lefthalf[i]
+            i=i+1
+            k=k+1
 
-        while j < len(M):
-            data[k] = M[j]
-            j += 1
-            k += 1
-
+        while j < len(righthalf):
+            data[k]=righthalf[j]
+            j=j+1
+            k=k+1
 
 
 # Do not change the following code
